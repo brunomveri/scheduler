046b31701716c1842056742fc20fb5947887eb34
brunomveri <mveri.bruno@gmail.com>
2021-02-16 12:50:05 -0700

    Added days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,6 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "components/Application.scss";
+import DayList from "components/DayList";
 
 export default function Application(props) {
   
@@ -21,6 +21,8 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+
+  const [day, setDay] = useState("Monday");
   
   return (
     <main className="layout">
@@ -31,7 +33,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
