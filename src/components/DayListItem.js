e15c49c7568b88e9882feb3e701d4f9adcd80459
brunomveri <mveri.bruno@gmail.com>
2021-02-15 21:34:04 -0700

    Created formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,24 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !(props.spots)
   });
+
+  function formatSpots(props) {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    if (props.spots === 2) {
+      return "2 spots remaining";
+    }
+    return props.spots;
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2>{props.name}</h2>
-      <h3>{props.spots}</h3>
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
 }
