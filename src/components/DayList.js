2abd53b3b1a0065bdfbd4a7c21f1e0b71bc2f283
brunomveri <mveri.bruno@gmail.com>
2021-02-16 14:54:43 -0700

    Added key into DayListItem, deleted days array, and changed days.map() into props.days.map()

---
+++ src/components/DayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+
+const DayList = (props) => {
+  
+  const mappedDays = props.days.map(day => {
+      return (
+        <DayListItem 
+          key={day.id}
+          id={day.id}
+          name={day.name} 
+          spots={day.spots} 
+          selected={day.name === props.day}
+          setDay={props.setDay}  
+        />
+      );
+  });
+  return (
+    <ul> 
+      {mappedDays}
+    </ul>
+  );
+}
+
+export default DayList;
