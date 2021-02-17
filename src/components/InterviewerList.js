6b138e87fe996b1cba74d85bce94b858077d823f
brunomveri <mveri.bruno@gmail.com>
2021-02-16 19:58:08 -0700

    Created InterviewerList and its stylesheet, added InterviewerListItem component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "components/InterviewerList.scss"
+import InterviewerListItem from "components/InterviewerListItem";
+
+const InterviewerList = (props) => {
+   
+  const mappedInterviewer = props.interviewers.map(interviewer => {
+    return(
+     <InterviewerListItem 
+       key={interviewer.id}
+      //  id={interviewer.id} //do I need this?
+       avatar={interviewer.avatar}
+       name={interviewer.name} 
+       selected={interviewer.id === props.interviewer}
+       setInterviewer={() => props.setInterviewer(interviewer.id)}
+     />
+    );
+  })
+    
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {mappedInterviewer}
+      </ul>
+    </section>
+  );
+};
+
+export default InterviewerList;
