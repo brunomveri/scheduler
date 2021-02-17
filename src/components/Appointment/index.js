23cf1fa38c287e40c0d7af2f0298fb76178d323b
brunomveri <mveri.bruno@gmail.com>
2021-02-17 16:13:30 -0700

    Changed function expression to function declaration

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss"
 
-const Appointment = (props) => {
+
+function Appointment (props) {
   
   return (
     <article className="appointment">
-
       
+
     </article>
   );
-};
+}
 
 export default Appointment;
