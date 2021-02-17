5393be2fa7eb83557813a277bb739d93bfb33f35
brunomveri <mveri.bruno@gmail.com>
2021-02-16 19:56:07 -0700

    Added props and InterviewerClass

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,1 +1,27 @@
 import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+const InterviewerListItem = (props) => {
+
+  const interviewerClass = classNames("interviewers__item", { /*??????*/
+    "interviewers__item--selected": props.selected
+  });
+
+  return (
+    <li
+      // id={props.id} //do I need this?
+      className=/*"interviewers__item"*/{interviewerClass}
+      onClick={props.setInterviewer}
+    >  
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
