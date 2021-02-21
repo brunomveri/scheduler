cb113e23f30fa641474136238bed0276f1691217
brunomveri <mveri.bruno@gmail.com>
2021-02-21 09:20:55 -0700

    Created save function, added onSubmit event handler to form element, changed onChange to setInterviewer in InterviewerList component, changed event handlers in Button component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,10 +17,14 @@ function Form(props) {
     props.onCancel()
   };
 
+  const save = () => {
+    props.onSave(name, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -28,19 +32,18 @@ function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={event => setName(event.target.value)}
-            //Add a reset() function to the Form component that calls setName("") and setInterviewer(null)
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
-          onChange={setInterviewer}
+          setInterviewer={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm /*onClick={}*/>Save</Button>
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
