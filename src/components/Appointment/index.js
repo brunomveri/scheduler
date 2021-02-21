f644c710943a47e7e35449c1d333b819f77b98f0
brunomveri <mveri.bruno@gmail.com>
2021-02-21 09:26:25 -0700

    Imported Form, Status, and useVisualMode. Created mode state, transition and back fuctions. Added conditional render components to Header component. Added Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,17 +2,53 @@ import React from "react";
 import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
+import Form from "./Form";
 import Empty from "./Empty";
+import Status from "components/Appointment/Status";
+import useVisualMode from "../../../src/hooks/useVisualMode";
+
+const CREATE = "CREATE";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const SAVING = "SAVING";
 
 function Appointment (props) {
   
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+    const save = (name, interviewer) => {
+      const interview = {
+        student: name,
+        interviewer,
+      };
+
+      transition(SAVING)
+      props.bookInterview(props.id, interview)
+        .then(() => {
+          transition(SHOW);
+        });
+
+    };
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview 
-        ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> 
-        : <Empty />
-      }
+        {mode === SAVING && <Status message="Saving..."/>}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+      {mode === CREATE &&
+        <Form 
+        interviewers={props.interviewers}
+        onCancel={back}
+        onSave={save}
+        />}
     </article>
   );
 }
