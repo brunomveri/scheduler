233c47d37764fc52ef721af098ad80b051b44b0b
brunomveri <mveri.bruno@gmail.com>
2021-02-25 13:31:49 -0700

    Changed props.interviewer.name to props.interviewer on h3 element

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -2,33 +2,33 @@ import React from "react";
 
 
 const Show = (props) => {
-  // console.log(props);
+  
   return (
     <main className="appointment__card appointment__card--show">
-  <section className="appointment__card-left">
-    <h2 className="text--regular">{props.student}</h2>
-    <section className="interviewer">
-      <h4 className="text--light">Interviewer</h4>
-      <h3 className="text--regular">{props.interviewer.name}</h3>
-    </section>
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <img
-        className="appointment__actions-button"
-        src="images/edit.png"
-        alt="Edit"
-        onClick={props.onEdit} //check with a mentor
-      />
-      <img
-        className="appointment__actions-button"
-        src="images/trash.png"
-        alt="Delete"
-        onClick={props.onDelete} //check with a mentor
-      />
-    </section>
-  </section>
-</main>
+      <section className="appointment__card-left">
+        <h2 className="text--regular">{props.student}</h2>
+        <section className="interviewer">
+          <h4 className="text--light">Interviewer</h4>
+          <h3 className="text--regular">{props.interviewer}</h3>
+        </section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <img
+            className="appointment__actions-button"
+            src="images/edit.png"
+            alt="Edit"
+            onClick={props.onEdit}
+          />
+          <img
+            className="appointment__actions-button"
+            src="images/trash.png"
+            alt="Delete"
+            onClick={props.onDelete}
+          />
+        </section>
+      </section>
+    </main>
   );
 };
 
