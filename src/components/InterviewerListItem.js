05b30a5fe1fa95d7d53bfb1cd96b85cee8a84c1c
brunomveri <mveri.bruno@gmail.com>
2021-02-25 13:43:21 -0700

    Changed onClick value to a callback. Organizes the code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,24 +4,43 @@ import classNames from "classnames";
 
 const InterviewerListItem = (props) => {
 
-  const interviewerClass = classNames("interviewers__item", { /*??????*/
+  const interviewerClass = classNames("day-interviewers__item", {
     "interviewers__item--selected": props.selected
   });
 
   return (
     <li
-      // id={props.id} //do I need this?
       className=/*"interviewers__item"*/{interviewerClass}
-      onClick={props.setInterviewer}
+      onClick={() => props.setInterviewer(props.name)}
     >  
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected && props.name}
+      {/* {props.selected && props.name} */}
+      {props.selected ? props.name : ''}
     </li>
   );
 };
 
 export default InterviewerListItem;
+
+// import React from "react";
+// import classnames from "classnames";
+// import "components/InterviewerListItem.scss";
+// export default function InterviewerListItem(props) {
+//   const interviewer = classnames("day-interviewers__item", {
+//     "interviewers__item--selected": props.selected
+//   })
+//   return (
+//     <li className={interviewer} onClick={() => props.setInterviewer(props.name)}>
+//     <img
+//       className="interviewers__item-image"
+//       src={props.avatar}
+//       alt={props.name}
+//     />
+//     {props.selected ? props.name : ''}
+//   </li>
+//   );
+// }
