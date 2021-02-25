33bb4569223bd89d97ffe4f756d9086e1bd7a501
brunomveri <mveri.bruno@gmail.com>
2021-02-25 09:53:14 -0700

    Added error state, created validate function, crated handleChange function, added section element, changed onClick={save} to onClick={validate} on Button element

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("")
@@ -17,8 +18,19 @@ function Form(props) {
     props.onCancel()
   };
 
-  const save = () => {
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    };
     props.onSave(name, interviewer);
+  };
+
+  const handleChange = (value) => {
+    if (error) {
+      setError("");
+    } 
+    setName(value);
   };
 
   return (
@@ -31,9 +43,10 @@ function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             value={name}
-            onChange={event => setName(event.target.value)}
+            onChange={event => handleChange(event.target.value)}
             data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -44,7 +57,7 @@ function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
