ee18153b8dd3ec463f32ed0c85ba7c1b036a53dd
brunomveri <mveri.bruno@gmail.com>
2021-02-25 14:41:47 -0700

    Organized the code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,7 +10,7 @@ const InterviewerListItem = (props) => {
 
   return (
     <li
-      className=/*"interviewers__item"*/{interviewerClass}
+      className={interviewerClass}
       onClick={() => props.setInterviewer(props.name)}
     >  
       <img
@@ -18,29 +18,9 @@ const InterviewerListItem = (props) => {
         src={props.avatar}
         alt={props.name}
       />
-      {/* {props.selected && props.name} */}
       {props.selected ? props.name : ''}
     </li>
   );
 };
 
 export default InterviewerListItem;
-
-// import React from "react";
-// import classnames from "classnames";
-// import "components/InterviewerListItem.scss";
-// export default function InterviewerListItem(props) {
-//   const interviewer = classnames("day-interviewers__item", {
-//     "interviewers__item--selected": props.selected
-//   })
-//   return (
-//     <li className={interviewer} onClick={() => props.setInterviewer(props.name)}>
-//     <img
-//       className="interviewers__item-image"
-//       src={props.avatar}
-//       alt={props.name}
-//     />
-//     {props.selected ? props.name : ''}
-//   </li>
-//   );
-// }
