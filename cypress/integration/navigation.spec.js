7f4e7e7f56b529fbf75685bb539d7cc64dc2466c
brunomveri <mveri.bruno@gmail.com>
2021-02-25 13:27:39 -0700

    Organized the code

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,20 +5,10 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    // cy.get("li").contains("Tuesday").click();
-    // cy.get("li")
-    //   .contains("li", "Tuesday")
-    //   .click()
-    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
     
     cy.contains("li", "Tuesday")
       .click()
       .should("have.css", "background-color", "rgb(242, 242, 242)");
-    
-      //Where to paste data-testid="day" ?
-      // cy.contains("[data-testid=day]", "Tuesday")
-      // .click()
-      // .should("have.class", "day-list__item--selected");
     
   });
 
